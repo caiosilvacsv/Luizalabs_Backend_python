10b728ad472a39c1da52c8d7810b0b995fe72838
Caio da Silva <caiosil.viana@gmail.com>
2026-03-26 12:58:47 -0300

    Adiciona exemplos de uso para as funções pop, remove, reverse, sort, len e sorted em lista.py

--- estrutura de dados/lista.py
+++ estrutura de dados/lista.py
@@ -95,3 +95,58 @@ def index_exemplo():
   numeros = [1, 30, 21, 2, 9, 65, 34, 30, 34]
   print(numeros.index(30)) # 1
   print(numeros.index(34)) # 6
+
+def pop_exemplo():
+  linguagens = ["python", "js", "java", "c", "c++", "c#"]
+  print(linguagens) # ['python', 'js', 'java', 'c', 'c++', 'c#']
+
+  linguagens.pop() #remove o ultimo item | c#
+  linguagens.pop() #remove o penultimo item | c++
+  linguagens.pop(0) #remove o primeiro item | python
+
+  print(linguagens) # ['js', 'java', 'c']
+
+def remove_exemplo():
+  linguagens = ["python", "js", "java", "java", "c", "c++", "c#"]
+  print(linguagens) # ['python', 'js', 'java', 'java', 'c', 'c++', 'c#']
+
+  linguagens.remove("java") #remove a primeira ocorrencia do  item | java
+  print(linguagens) # ['python', 'js', 'java', 'c', 'c++', 'c#']
+
+
+def reverse_exemplo():
+  linguagens = ["python", "js", "java", "java", "c", "c++", "c#"]
+  print(linguagens) # ['python', 'js', 'java', 'java', 'c', 'c++', 'c#']
+  
+  linguagens.reverse()
+  print(linguagens) # ['c#', 'c++', 'c', 'java', 'java', 'js', 'python']
+
+  #dá pra ser feito com o slice (fatiamento)
+  print(linguagens[::-1]) # ['python', 'js', 'java', 'java', 'c', 'c++', 'c#']
+
+def sort_exemplo():
+  linguagens = ["python", "js", "java", "java", "c", "c++", "c#"]
+
+  linguagens.sort() #ordena a lista por ordem alfabetica
+  print(linguagens) # ['c', 'c++', 'c#', 'java', 'java', 'js', 'python']
+
+  linguagens.sort(reverse=True) # ordena a lista por ordem alfabetica invertida
+  print(linguagens) # ['python', 'js', 'java', 'java', 'c#', 'c++', 'c']
+
+  linguagens.sort(key=lambda x: len(x)) #ordena a lista por tamanho da palavra
+  print(linguagens) # ['c', 'js', 'c#', 'c++', 'java', 'java', 'python']
+  
+  linguagens.sort(key=lambda x: len(x), reverse=True) #ordena a lista por tamanho da palavra invertida
+  print(linguagens) # ['python', 'java', 'java', 'c++', 'c#', 'js', 'c']
+
+def len_exemplo():
+  linguagens = ["python", "js", "java", "java", "c", "c++", "c#"]
+
+  print(len(linguagens)) # 7
+
+def sorted_exemplo():
+  linguagens = ["python", "js", "java", "java", "c", "c++", "c#"]
+
+  print(sorted(linguagens, key=lambda x: len(x))) # ['c', 'js', 'c#', 'c++', 'java', 'java', 'python']
+  print(sorted(linguagens, key=lambda x: len(x), reverse=True)) # ['python', 'java', 'java', 'c++', 'js', 'c#', 'c']
+  print(sorted(linguagens))  # ['c', 'js', 'c#', 'c++', 'java', 'java', 'python']
